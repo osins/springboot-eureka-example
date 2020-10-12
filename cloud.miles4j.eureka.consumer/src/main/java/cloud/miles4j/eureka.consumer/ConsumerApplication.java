95f4ed1a8d11ba501e28d87f58c59cbc295b2cfb
wangsying <wangsying@gmail.com>
2020-10-12 18:48:00 +0800

    project init

---
+++ cloud.miles4j.eureka.consumer/src/main/java/cloud/miles4j/eureka.consumer/ConsumerApplication.java
@@ -0,0 +1,63 @@
+package cloud.miles4j.eureka.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Eureka客户端-消费者
+ */
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class ConsumerApplication {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+
+    /**
+     * 实例化RestTemplate
+     *
+     * @return
+     */
+    @LoadBalanced
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
+
+
+    /**
+     * Rest服务端使用RestTemplate发起http请求,然后得到数据返回给前端----gotoUser是为了区分getUser怕小伙伴晕头
+     *
+     * @param id
+     * @return
+     **/
+    @GetMapping(value = "/getUser")
+    @ResponseBody
+    public Map<String, Object> getUser(@RequestParam Integer id) {
+        Map<String, Object> data = new HashMap<>();
+
+        data = restTemplate.getForObject("http://cloud.miles4j.eureka.provider/getUser?id=" + id, Map.class);
+
+        return data;
+
+    }
+}
